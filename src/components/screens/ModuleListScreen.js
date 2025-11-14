7498ab98549935c74ca185635786250421404178
Yasiin-Adaw <yasinadaw1@gmail.com>
2025-11-14 02:13:48 +0000

    Uncompleted activity 4 with error (#6)

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,6 +1,7 @@
 
-import {Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
 import Screen from '../layout/Screen';
+import ModuleItem from '../entity/modules/ModuleItem';
 
 import initialModules from '../../data/modules.js';
 
@@ -16,16 +17,9 @@ export const ModuleListScreen = () => {
      
         <ScrollView style={styles.container}>
         {modules.map((module) => {
-        return (
-          <Pressable key={module.ModuleCode} onPress={handleSelect}>
-          <View >
-            <Text>
-              {module.ModuleCode} {module.ModuleName} 
+        
 
-            </Text>
-          </View>
-          </Pressable>
-        )
+        return <ModuleItem key={module.ModuleCode} module={module} onSelect={handleSelect} />;
 
 
       })}
@@ -37,14 +31,7 @@ export const ModuleListScreen = () => {
 
 const styles = StyleSheet.create({
   container: {},
-  item: {
-    paddingVertical: 15,
-    borderTopWidth: 1,
-    borderColor: 'lightgray',
-  },
-  text: {
-    ontSize: 16,
-  },
+  
   
 });
 export default ModuleListScreen;
