abc1ecc184f5ddb7a07a3ac9c260c32d78ee0d20
Yasiin-Adaw <yasinadaw1@gmail.com>
2025-12-02 04:56:04 +0000

    completed Activity-11-Extracting-reusable-form-components (#13)

---
+++ src/components/entity/modules/ModuleForm.js
@@ -0,0 +1,87 @@
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import Icons from '../../UI/Icons';
+import Form from '../../UI/Form';
+
+const defaultModule = {
+    ModuleID: null,
+    ModuleCode: null,
+    ModuleName: null,
+    ModuleLevel: null,
+    ModuleLeaderID: null,
+    ModuleLeaderName: null,
+    ModuleImage: null,
+};
+
+const ModuleForm = ({ onSubmit, onCancel }) => {
+    // Initialisations ----------------------
+    defaultModule.ModuleID = Math.floor(Math.random() * 900000) + 100000;
+    defaultModule.ModuleImage = 'https://images.freeimages.com/images/small-preview/cf5/cellphone-1313194.jpg';
+
+    const levels = [
+        { value: 3, label: '3 (Foundation)' },
+        { value: 4, label: '4 (First year)' },
+        { value: 5, label: '5 (Second year)' },
+        { value: 6, label: '6 (Final year)' },
+        { value: 7, label: '7 (Masters)' },
+    ];
+
+
+
+    // State ----------------------------
+    const [module, setModule] = useState(defaultModule);
+
+    // Handlers -----------------------
+    const handleChange = (field, value) => setModule({ ...module, [field]: value });
+    const handleSubmit = () => onSubmit(module);
+    // View -----------------------------
+    const submitLabel = 'Add';
+    const submitIcon = <Icons.Add />;
+    return (
+        <Form
+            onSubmit={handleSubmit}
+            onCancel={onCancel}
+            submitLabel={submitLabel}
+            submitIcon={submitIcon}
+
+        >
+            <Form.InputText
+                label="Module code"
+                value={module.ModuleCode}
+                onChange={(value) => handleChange('ModuleCode', value)} />
+
+            <Form.InputText
+                label="Module name"
+                value={module.ModuleName}
+                onChange={(value) => handleChange('ModuleName', value)} />
+
+            <Form.InputSelect
+                label="Module level"
+                prompt="Select module level ..."
+                options={levels}
+                value={module.ModuleLevel}
+                onChange={(value) => handleChange('ModuleLevel', value)} />
+
+            <Form.InputText
+                label="Module leader"
+                value={module.ModuleLeaderName}
+                onChange={(value) => handleChange('ModuleLeaderName', value)} />
+
+            <Form.InputText
+                label="Module image URI"
+                value={module.ModuleImage}
+                onChange={(value) => handleChange('ModuleImage', value)} />
+
+
+
+        </Form>
+
+    );
+};
+
+const styles = StyleSheet.create({
+
+
+})
+
+export default ModuleForm;
